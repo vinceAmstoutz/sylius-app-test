3d92059d2860f1c2ccd4e9fa2abbca520bc932d5
Vincent Amstoutz <vincent.amstoutz@les-tilleuls.coop>
2025-01-15 17:50:27 +0100

    chore(deps-php): add symfony/maker for dev

--- config/bundles.php
+++ config/bundles.php
@@ -60,4 +60,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Sylius\TwigExtra\Symfony\SyliusTwigExtraBundle::class => ['all' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
