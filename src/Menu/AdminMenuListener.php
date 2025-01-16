3d21e0b7794b8174eb9da15cb2d5fd4d864bd64f
Vincent Amstoutz <vincent.amstoutz@les-tilleuls.coop>
2025-01-16 16:44:46 +0100

    feat(admin/menu): add custom menu for suppliers list

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Menu;
+
+use Sylius\Bundle\AdminBundle\Menu\MainMenuBuilder;
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+#[AsEventListener(MainMenuBuilder::EVENT_NAME)]
+final class AdminMenuListener
+{
+    public function __invoke(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $suppliersItem = $menu->addChild('suppliers')
+            ->setLabel('Suppliers')
+            ->setLabelAttribute('icon', 'tabler:box')
+        ;
+
+        $suppliersItem
+            ->addChild('admin_supplier', [
+                'route' => 'app_admin_supplier_index',
+            ])
+            ->setAttribute('type', 'link')
+            ->setLabel('Suppliers')
+        ;
+    }
+}
